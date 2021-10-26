d25b07408e8db1d2281c60c2b39e081a7b79dc06
JoYoungWook 조영욱 <siosio24@naver.com>
2021-10-26 15:55:15 +0900

    jquery and CRUD

--- src/main/java/com/example/demo/Controller/IndexController.java
+++ src/main/java/com/example/demo/Controller/IndexController.java
@@ -1,20 +1,33 @@
 package com.example.demo.Controller;
 
+import com.example.demo.service.posts.PostsService;
+import com.example.demo.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
         return "/test/index";
     }
     @GetMapping("/posts/save")
     public String postsSave(){
         return "/test/posts-save";
+    }
+    @GetMapping("/posts/update/{id}")
+    public String postUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto post = postsService.findById(id);
+        model.addAttribute("post", post);
+        return "/test/posts-update";
     }
 
 }
--- src/main/java/com/example/demo/service/posts/PostsService.java
+++ src/main/java/com/example/demo/service/posts/PostsService.java
@@ -2,12 +2,17 @@ package com.example.demo.service.posts;
 
 import com.example.demo.domain.posts.Posts;
 import com.example.demo.domain.posts.PostsRepository;
+import com.example.demo.web.dto.PostsListResponseDto;
 import com.example.demo.web.dto.PostsResponseDto;
 import com.example.demo.web.dto.PostsSaveRequestDto;
 import com.example.demo.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.apache.bcel.generic.LOOKUPSWITCH;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,5 +33,18 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).
+                orElseThrow(()->new IllegalArgumentException("해당 게시글이 업습니다."));
+        postsRepository.delete(posts);
     }
 }
